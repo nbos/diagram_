9e899980e0671a91b7fa1fc109852647f3266f4c
nbos <35620093+nbos@users.noreply.github.com>
2025-05-07 06:44:09 -0400

    Rules maintain the inverse-constructive-suffix relation

--- src/Diagram/Rules.hs
+++ src/Diagram/Rules.hs
@@ -4,25 +4,31 @@
 module Diagram.Rules (module Diagram.Rules) where
 
 import Prelude hiding (length)
+
+import Control.Monad (forM_)
+import Control.Monad.ST (runST)
 import Control.Exception (assert)
 
 import Data.Word (Word8)
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.UTF8 as UTF8
+import qualified Data.Vector as B
 import qualified Data.Vector.Unboxed as U
 import qualified Data.Vector.Generic as V
+import qualified Data.Vector.Generic.Mutable as MV
 
 import Diagram.Util
 
-newtype Rules = Rules {
+data Rules = Rules {
   -- | Self-referrential vector of recipes for the construction of all
   -- symbols above 256 indexed at s-256
-  invRules :: U.Vector (Int,Int) -- s01 -> (s0,s1)
+  invRules :: !(U.Vector (Int,Int)), -- s01 -> (s0,s1)
+  sufChildren :: !(B.Vector [Int]) -- s1 -> [s01]
 }
 
 -- | New empty rule set
 empty :: Rules
-empty = Rules U.empty
+empty = Rules V.empty $ V.replicate 256 []
 
 -- | Number of rules
 size :: Rules -> Int
@@ -34,25 +40,37 @@ numSymbols = (256 +) . size
 
 -- | Compute the length of a symbol
 symbolLength :: Rules -> Int -> Int
-symbolLength (Rules irs) = go
+symbolLength (Rules irs _) = go
   where
     go s | s < 256   = 1
          | otherwise = let (s0,s1) = irs V.! (s - 256)
                        in go s0 + go s1
 
 fromList :: [(Int, Int)] -> Rules
-fromList = Rules . U.fromList
+fromList l = Rules irs scs
+  where
+    irs = V.fromList l
+    sz = V.length irs
+    scs = runST $ do
+      mscs <- MV.replicate (256 + sz) []
+      forM_ (zip l [256..]) $ \((s0,s1),s01) ->
+        assert (s0 < s01 && s1 < s01)
+        MV.modify mscs (s01:) s1
+      V.freeze mscs
 
 -- | Add a new symbol with a construction rule. Returns updated rules
 -- and index of new symbol. O(n)
 push :: (Int, Int) -> Rules -> (Int, Rules)
-push s0s1 rs@(Rules irs) = assert (both (< s01) s0s1)
-                           (s01, Rules $ V.snoc irs s0s1)
-  where s01 = numSymbols rs
+push s0s1@(_,s1) rs@(Rules irs scs) = assert (both (< s01) s0s1)
+                                      (s01, Rules irs' scs')
+  where
+    s01 = numSymbols rs
+    irs' = V.snoc irs s0s1
+    scs' = V.modify (flip2 MV.modify (s01:) s1) scs
 
 -- | Lookup the rule for constructing a given symbol
 (!) :: Rules -> Int -> (Int,Int)
-(!) (Rules irs) s = irs V.! (s - 256)
+(!) (Rules irs _) s = irs V.! (s - 256)
 infixl 9 !
 
 -- | Lookup the rule for constructing a given symbol. Nothing returned
@@ -61,9 +79,14 @@ invLookup :: Int -> Rules -> Maybe (Int,Int)
 invLookup s rs | s < 256   = Nothing
                | otherwise = Just $ rs ! s
 
+-- | Return all symbols that have the given symbols as a right symbol
+-- (s1) in an unspecified order (reverse symbol index as value for now)
+sChildren :: Int -> Rules -> [Int]
+sChildren s (Rules _ scs)= scs V.! s
+
 -- | Deconstruct a symbol back into a list of bytes
 extension :: Rules -> Int -> [Word8]
-extension (Rules irs) = go
+extension (Rules irs _) = go
   where
     go s | s < 256 = [toEnum s]
          | otherwise = let (s0,s1) = irs V.! (s - 256)
@@ -73,7 +96,7 @@ extension (Rules irs) = go
 -- symbol, from large to small, starting with the symbol itself and
 -- ending with the first atomic symbol of its extension
 cPrefixes :: Rules -> Int -> [Int]
-cPrefixes (Rules irs) = go
+cPrefixes (Rules irs _) = go
   where
     go s | s < 256 = [s]
          | otherwise = let (s0,_) = irs V.! (s - 256)
@@ -83,7 +106,7 @@ cPrefixes (Rules irs) = go
 -- symbol, from large to small, starting with the symbol itself and
 -- ending with the last atomic symbol of its extension
 cSuffixes :: Rules -> Int -> [Int]
-cSuffixes (Rules irs) = go
+cSuffixes (Rules irs _) = go
   where
     go s | s < 256 = [s]
          | otherwise = let (_,s1) = irs V.! (s - 256)
