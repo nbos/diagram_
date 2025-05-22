3d6b1c573ac48f0f033aadcf6a78213a3f2b1c9f
nbos <35620093+nbos@users.noreply.github.com>
2025-05-22 09:04:36 -0400

    Progress on candidates bookkeeping

--- src/Diagram.hs
+++ src/Diagram.hs
@@ -4,64 +4,48 @@
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE RankNTypes #-}
 
 module Diagram (module Diagram) where
 
--- import Control.Exception (assert)
+import Control.Lens hiding (pre)
+import Control.Monad.ST
 
--- import Control.Monad
-import Control.Lens
-import Control.Monad.State.Strict
-import Control.Monad.Primitive (PrimMonad)
-
--- import Data.Bifunctor
-
--- import Data.Word
+import Data.STRef
 import qualified Data.List as L
-import Data.IntMap.Strict (IntMap)
-import qualified Data.IntMap.Strict as IM
+
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
--- import Data.ByteString (ByteString)
--- import qualified Data.ByteString as BS
+import qualified Data.IntMap.Strict as IM
 
--- import Streaming
-import qualified Streaming.Prelude as S
--- import qualified Diagram.Streaming as S
-
+import Diagram.Rules (Rules)
 import qualified Diagram.Rules as R
-import Diagram.Model (Model(..))
+import Diagram.Model (Model)
 import qualified Diagram.Model as Mdl
--- import qualified Diagram.Trie as Trie
+
 import Diagram.Util
 
 err :: [Char] -> a
 err = error . ("Diagram: " ++)
 
-type Search m = StateT (SearchState m) m
-data SearchState m = Search {
-  _sModel :: !(Model m),
+data Search = Search {
+  _sModel :: !Model,
   _sTargets :: ![Int],
-
-  -- | We can keep track of all the edges that would be eliminated from a
-  -- new rule (s0,s1) by counting the number of times each construction
-  -- prefix (c-prefix) of s1 (from itself down to the first atom of its
-  -- extension) is introduced how many times by an extension suffix
-  -- (e-suffix) of s0.
-  _sCandidates :: !(Map (Int,Int) (IntMap Int)) -- (s0,s1) -> pre1 -> n
+  _sCandidates :: !(Map (Int,Int) -- (s0,s1) -> 
+                    ((Maybe Int, Int), Int)) -- ((suf0, pref1), n)
 }
-makeLenses ''SearchState
+makeLenses ''Search
 
 -- | Construction
-fromString :: PrimMonad m => [Int] -> m (SearchState m)
-fromString ss = do
-  mdl <- Mdl.singleton counts
+fromString :: [Int] -> Search
+fromString ss =
+  let mdl = Mdl.singleton counts
   -- ctxs <- forM ss $ DMV.read $ mdl^.modelKeys
-  -- let events = (BS.empty, hd) : zip ctxs tl
-  return $ Search mdl ss candidates
+  -- let events = (BS.empty, hd) : zip ctxs tl 
+  in Search mdl ss candidates
   where
     counts = L.foldl' (\m s -> IM.insertWith (+) s 1 m) IM.empty ss
-    candidates = M.mapWithKey (\(_,s1) n -> IM.singleton s1 n) $
+    candidates = M.mapWithKey (\(_,s1) n -> ((Nothing,s1),n)) $
                  countJoints ss
 
 -- | Linear space traversal of the list with all previous (in reverse)
@@ -77,52 +61,108 @@ countJoints [] = M.empty
 countJoints ss@(_:tl) = L.foldl' f M.empty $ zip ss tl
   where f m s0s1 = M.insertWith (+) s0s1 1 m
 
-type Prediction = (Maybe Int, Int)
-type Candidates = Map (Int,Int) (IntMap Int)
+type Candidates = Map (Int,Int) ((Maybe Int, Int), Int)
 
-updateCandidates :: forall m. PrimMonad m => Int -> (Int, Int) ->
-                    Model m -> [Prediction] -> Candidates ->
-                    m ([Prediction], Candidates)
-updateCandidates s01 (s0,s1) mdl@(Model rs _ _ctxTrie) = go []
+-- | Augment a predecessor prediction map into a full "Candidates" map
+-- by counting all possible constructions from the list of
+-- predictions. This is not part of the search algorithm but defines the
+-- invariant preserved when counts are updated by a pass over the string
+-- after a rule introduction
+fromPredictions :: Map (Int,Int) (Maybe Int, Int) -> Rules ->
+                   [(Maybe Int, Int)] -> Candidates
+fromPredictions pps _ [] = (,0) <$> pps
+fromPredictions _ _ ((Just _,_):_) = err "impossible"
+fromPredictions pps rs ((Nothing,ctx0):pdns) = runST $ do
+  m <- newSTRef $ (minBound :: Int,) . (,0) <$> pps
+  go m 0 ctx0 pdns
+  snd <<$>> readSTRef m -- discard idxs
   where
-    eMatch = Mdl.eMatchRev mdl
-    cMatch = Mdl.cMatchRev mdl
+    -- | increment by 1 each from the cartesian product of symbols to
+    -- the left greater or equal to the current context and symbols to
+    -- the right greater or equal to the current target except the
+    -- current pairing (minCtx,tgt)
+    go :: STRef s (Map (Int,Int) (Int, ((Maybe Int,Int), Int))) ->
+          Int -> Int -> [(Maybe Int,Int)] -> ST s ()
+    go _ _ _ [] = return () -- end
+    go m i maxCtx ((minCtx,tgt):rest) = do
+      sequence_ [ inc m i s0 s1 | s0 <- left
+                                , s1 <- right
+                                , Just s0 /= minCtx || s1 /= tgt ]
 
-    go :: [Prediction] -> [Prediction] ->
-          Candidates -> m ([Prediction], Candidates)
-    go bwd [] = return . (reverse bwd, )
-    go bwd (pr@(_ctx,_s):fwd) = \m -> cMatch s1 (snd <$> bwd') >>= \case
+      go m i' maxHead pdns' -- continue
 
-      Nothing -> eMatch s01 (snd <$> bwd') >>= \case
-        Nothing -> doesntMatch m
-        Just _ -> matchesAsCtx m
+        where
+          -- filter (minCtx <=) (maxCtxB:[maxCtx])
+          left = maybe id (filter . (<=)) minCtx $
+                 maybe id ((:) . snd) (R.invLookup rs maxCtx)
+                 [maxCtx]
+            -- case R.invLookup rs maxCtx of
+            -- Nothing -> [maxCtx]
+            -- Just (_, maxCtxB) -> case minCtx of
+            --   Nothing -> [maxCtxB, maxCtx]
+            --   Just minCtxS | minCtxS == maxCtxB -> [maxCtxB, maxCtx]
+            --                | minCtxS == maxCtx -> [maxCtx]
+            --                | otherwise -> err "impossible"
 
-      Just (cPref1, bwdRest) -> eMatch s0 bwdRest >>= \case
-        Nothing -> doesntMatch m
+          maxHead = R.maxHead rs tgt rest
+          right = takeUntil (== tgt) $
+                  R.prefixes rs maxHead
 
-        -- s0 ext, s1 cons. match: ensure no claim supersedes s0's
-        Just _bwdAtoms -> do
-          let _lenPref1s = length cPref1
+          headLen = length right
+          i' = i + headLen
+          pdns' = drop (headLen - 1) rest
 
-          -- bs0 <- Mdl.ctxKey mdl s0
-          -- let len0 = BS.length bs0
-          -- (hd, msts, tl) <- Trie.resolveGeneric bs0 ctxTrie bwdAtoms
-          -- let revRest' = S.each (L.drop len0 $ BS.unpack hd) >> tl
-          -- case L.find (flip Mdl.claims s1 . snd) $ reverse msts of
-          --   Nothing -> findErr msts
-          --   Just (Just s', _) | s' > s0 -> matchesAsCtx m
-          --   Just (claimant, _) -> do
-          
-          undefined -- TODO
+    -- | increment (s0,s1) candidate by 1, if constructable
+    inc :: STRef s (Map (Int,Int) (Int, ((Maybe Int,Int), Int))) ->
+           Int -> Int -> Int -> ST s ()
+    inc m i s0 s1 =
+      modifySTRef m $
+      flip M.adjust (s0,s1) $ \e@(lastChunked,(pp@(_,pre1),n)) ->
+      let -- [s1..]pre1 (null iff s1 == pre1)
+          inter1 = takeWhile (/= pre1) $ R.prefixes rs s1
+          constructable = i - length inter1 > lastChunked
+      in if constructable then (i,(pp,n+1)) else e
 
+
+updateCandidates :: Rules -> Int -> (Int, Int) -> (Maybe Int, Int) ->
+                    Candidates -> [(Maybe Int, Int)] ->
+                    (Candidates, [(Maybe Int, Int)])
+updateCandidates rs s01 (s0,s1) (preCtx,ppp1) cdts pdns = runST $ do
+  m <- newSTRef cdts
+  pdns' <- go m [] pdns
+  cdts' <- readSTRef m
+  return (cdts', pdns')
+  
+  where
+    go :: STRef s Candidates -> [(Maybe Int, Int)] ->
+          [(Maybe Int, Int)] -> ST s [(Maybe Int, Int)]
+    go _ bwd [] = return $ reverse bwd -- end
+    go m [] (pdn0:fwd) = go m [pdn0] fwd
+    go m bwd@(maxCtx@(ctxA,ctxB):bwdRest) fwd@(next@(minCtx,tgt):fwdRest)
+      | match0 && match1 = undefined
+
+      | otherwise = undefined
       where
-        doesntMatch = go bwd' fwd
-        matchesAsCtx _m = undefined -- TODO
+        -- only two cases match s0: s0 is the previous target or s0 is
+        -- the previous prediction (s0B target in the s0A context)
+        match0 = ctxB == s0 || case R.invLookup rs s0 of
+          Nothing -> False
+          Just (s0A,s0B) -> ctxA == Just s0A && ctxB == s0B
+
+        match1 = -- we could compare full (Maybe Int, Int) but targets +
+                 -- pp fully determine contexts so it's unnecessary
+          (snd <$> fwd1) == pattern1
         
-        bwd' = pr:bwd 
+        maxHead = R.maxHead rs tgt fwd --------- might go beyond s1
+        
+        fwd1 = take len1 fwd
+        rightSymbols = R.resolveFwd rs fwdRest
 
-        _fwdAtoms = S.mapM_ (R.extension rs) undefined -- $ S.each fwd
+    -- [pre1..s1] (not null)
+    pattern1 = assertId (not . null) $
+               reverse $
+               takeUntil (== ppp1) $
+               R.prefixes rs s1
 
-        _findErr lin = err $ "updateCandidates: Can't find symbol "
-          ++ show s1 ++ " in context lineage: " ++ show lin
+    len1 = length pattern1
 
