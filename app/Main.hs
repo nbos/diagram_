26f290be74ba497703abaf78842b24fc58011dc7
nbos <35620093+nbos@users.noreply.github.com>
2025-07-27 09:40:56 -0400

    Clean

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,3 @@
-module Main where
-
-import System.Environment (getArgs)
-import System.Exit (die)
-import qualified Data.ByteString as BS
-
-import Diagram ()
-
-main :: IO ()
-main = do
-  args <- getArgs
-  case args of
-    [filePath] -> do
-      putStrLn "Reading file..."
-      _bs <- BS.readFile filePath
-      return ()
-
-    _other -> die "Usage: program <file-path>"
+module Main (main) where
+import Diagram (main)
 
